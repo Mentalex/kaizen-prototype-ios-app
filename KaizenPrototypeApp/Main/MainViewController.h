6603e955ca543f3fa677f8d088e44fbff19e2f4a
Alex Tapia <alejandrovztapia@gmail.com>
2022-05-28 10:02:55 -0700

    Added `MainViewController` where the Collection View will be implemented to show gallery of photos.

---
+++ KaizenPrototypeApp/Main/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  KaizenPrototypeApp
+//
+//  Created by Alex Tapia on 28/05/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
